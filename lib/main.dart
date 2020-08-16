a949af9f6fbb3fc2b61e2639882bbe09c5f17504
Mohammed Sadiq <sadiqmmm@gmail.com>
2020-08-16 15:01:08 +0530

    Add the state full widget with function

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,29 +16,53 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
+  void changeDiceFace() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+
+    // print("Dice Left Number:= $leftDiceNumber");
+    // print("Dice Right Number:= $rightDiceNumber");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.blue,
       appBar: AppBar(
         title: Text("Dicee"),
         centerTitle: true,
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.black,
       ),
       body: Center(
         child: Row(
           children: [
             Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Image.asset("images/dice1.png"),
+              child: FlatButton(
+                child: Image.asset("images/dice$leftDiceNumber.png"),
+                onPressed: () {
+                  setState(() {
+                    changeDiceFace();
+                  });
+                },
               ),
             ),
             Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Image.asset("images/dice2.png"),
+              child: FlatButton(
+                child: Image.asset("images/dice$rightDiceNumber.png"),
+                onPressed: () {
+                  setState(() {
+                    changeDiceFace();
+                  });
+                },
               ),
             ),
           ],
